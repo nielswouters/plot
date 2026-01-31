e45c34e41332fedea91c9fbedbad7693bca817f7
Niels Wouters <niels.wouters@otoy.com>
2026-01-31 20:47:40 +0100

    using SDL back buffer with known format

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+const SDL_PixelFormat BACK_BUFFER_FORMAT = SDL_PIXELFORMAT_ARGB8888;
+
 static void PrintSDLError(const char* _Message)
 {
     printf("%s. SDL_Error: %s\n", _Message, SDL_GetError());
@@ -38,70 +40,123 @@ int main()
         return -1;
     }
 
-    SDL_Window* window = window = SDL_CreateWindow("Plot", SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    SDL_Window* window = SDL_CreateWindow("Plot", SCREEN_WIDTH, SCREEN_HEIGHT, 0);
     if (!window)
     {
         PrintSDLError("SDL could not create a window");
+        SDL_Quit();
+        return -1;
     }
-    else
+
+    SDL_Surface* screen = SDL_GetWindowSurface(window);
+    if (!screen)
     {
-        SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
+        PrintSDLError("SDL_GetWindowSurface failed");
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
 
-        Plot plot;
+    SDL_Surface* backbuffer = SDL_CreateSurface(screen->w, screen->h, BACK_BUFFER_FORMAT);
+    if (!backbuffer)
+    {
+        PrintSDLError("SDL_CreateSurface(backbuffer) failed");
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
 
-        bool run = true;
-        while (run)
+    SDL_SetSurfaceBlendMode(backbuffer, SDL_BLENDMODE_NONE);
+
+    Plot plot;
+    bool run = true;
+
+    while (run)
+    {
+        SDL_Event e;
+        while (SDL_PollEvent(&e))
         {
-            if (!SDL_UpdateWindowSurface(window))
+            switch (e.type)
             {
-                PrintSDLError("SDL_UpdateWindowSurface failed");
+            case SDL_EVENT_QUIT:
                 run = false;
-            }
-            else
+                break;
+
+            case SDL_EVENT_WINDOW_RESIZED:
+            case SDL_EVENT_WINDOW_PIXEL_SIZE_CHANGED:
             {
-                SDL_Event e;
-                while (SDL_PollEvent(&e))
+                screen = SDL_GetWindowSurface(window);
+                if (!screen)
                 {
-                    switch (e.type)
-                    {
-                    case SDL_EVENT_QUIT:
-                        run = false;
-                        break;
-
-                    case SDL_EVENT_WINDOW_RESIZED:
-                        screenSurface = SDL_GetWindowSurface(window);
-                        break;
-
-                    case SDL_EVENT_MOUSE_MOTION:
-                        plot.MouseMove((uint32_t)e.motion.x, (uint32_t)e.motion.y);
-                        break;
-
-                    case SDL_EVENT_MOUSE_BUTTON_UP:
-                    case SDL_EVENT_MOUSE_BUTTON_DOWN:
-                        plot.MouseButton(e.button.button, e.button.down);
-                        break;
-
-                    case SDL_EVENT_KEY_UP:
-                    case SDL_EVENT_KEY_DOWN:
-                        plot.KeyboardButton(e.key.key, e.key.down);
-                        break;
-                    }
+                    PrintSDLError("SDL_GetWindowSurface failed after resize");
+                    run = false;
+                    break;
                 }
 
-                float deltaTime = DeltaTime();
-                plot.Update(deltaTime);
-                
-                // assert(screenSurface->format == SDL_PIXELFORMAT_XRGB8888);
-                Surface surface((uint32_t*)screenSurface->pixels, screenSurface->w, screenSurface->h, screenSurface->pitch / 4);
+                SDL_DestroySurface(backbuffer);
+                backbuffer = SDL_CreateSurface(screen->w, screen->h, BACK_BUFFER_FORMAT);
+                if (!backbuffer)
+                {
+                    PrintSDLError("SDL_CreateSurface(backbuffer) failed after resize");
+                    run = false;
+                    break;
+                }
 
-                plot.Draw(&surface);
+                SDL_SetSurfaceBlendMode(backbuffer, SDL_BLENDMODE_NONE);
+            } break;
+
+            case SDL_EVENT_MOUSE_MOTION:
+                plot.MouseMove((uint32_t)e.motion.x, (uint32_t)e.motion.y);
+                break;
+
+            case SDL_EVENT_MOUSE_BUTTON_UP:
+            case SDL_EVENT_MOUSE_BUTTON_DOWN:
+                plot.MouseButton(e.button.button, e.button.down);
+                break;
+
+            case SDL_EVENT_KEY_UP:
+            case SDL_EVENT_KEY_DOWN:
+                plot.KeyboardButton(e.key.key, e.key.down);
+                break;
             }
         }
 
-        SDL_DestroyWindow(window);
+        if (!run) break;
+
+        float deltaTime = DeltaTime();
+        plot.Update(deltaTime);
+
+        if (!SDL_LockSurface(backbuffer))
+        {
+            PrintSDLError("SDL_LockSurface failed");
+            run = false;
+            break;
+        }
+
+        Surface surface((uint32_t*)backbuffer->pixels,
+            backbuffer->w, backbuffer->h,
+            backbuffer->pitch / 4);
+        plot.Draw(&surface);
+
+        SDL_UnlockSurface(backbuffer);
+
+        if (!SDL_BlitSurface(backbuffer, NULL, screen, NULL))
+        {
+            PrintSDLError("SDL_BlitSurface failed");
+            run = false;
+            break;
+        }
+
+        if (!SDL_UpdateWindowSurface(window))
+        {
+            PrintSDLError("SDL_UpdateWindowSurface failed");
+            run = false;
+            break;
+        }
     }
 
+    SDL_DestroySurface(backbuffer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
-
     return 0;
 }

63ebffb1630ead884791636abcf1996bb1caaf06
Niels Wouters <niels.wouters@otoy.com>
2026-01-31 20:30:14 +0100

    Cleaned up cmake helpers

--- clean.sh
+++ clean.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e
+
 rm -rf .vs
 rm -rf bin
 rm -rf build
--- generate.sh
+++ generate.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+set -e
 
-# Create the build directory
+while [[ $# -gt 0 ]]
+do
+    case "$1" in
+        -config)
+            CONFIG="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            exit 1
+            ;;
+    esac
+done
+
+CONFIG="$(tr '[:lower:]' '[:upper:]' <<< "${CONFIG:0:1}")${CONFIG:1}"
+
 mkdir -p build
-
-# Change into the build directory
 cd ./build
 
-# Run CMake to generate the solution
-cmake .. -DCMAKE_BUILD_TYPE=Debug
+cmake .. -DCMAKE_BUILD_TYPE="$CONFIG"
